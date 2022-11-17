f70dde59baf05ba6308b282d850b1e582c130470
Gabriel Sanchez <jogasoftware07@gmail.com>
2022-11-16 22:20:30 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="JogaSoftware Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+        <b>JogaSoftware</b>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <HomePage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HomePage from './components/homePage';
+
+export default {
+  name: 'App',
+
+  components: {
+    HomePage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/homePage.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <!-- <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col> -->
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          JogaSoftware
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-row dense>
+          <v-col cols="12">
+            <v-card
+              color="#385F73"
+              dark
+            >
+              <v-card-title class="text-h5">
+                Unlimited music now
+              </v-card-title>
+  
+              <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+  
+              <v-card-actions>
+                <v-btn text>
+                  Listen Now
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+  
+          <v-col
+            v-for="(item, i) in items"
+            :key="i"
+            cols="12"
+          >
+            <v-card
+              :color="item.color"
+              dark
+            >
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title
+                    class="text-h5"
+                    v-text="item.title"
+                  ></v-card-title>
+  
+                  <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+  
+                  <v-card-actions>
+                    <v-btn
+                      v-if="item.artist === 'Ellie Goulding'"
+                      class="ml-2 mt-3"
+                      fab
+                      icon
+                      height="40px"
+                      right
+                      width="40px"
+                    >
+                      <v-icon>mdi-play</v-icon>
+                    </v-btn>
+  
+                    <v-btn
+                      v-else
+                      class="ml-2 mt-5"
+                      outlined
+                      rounded
+                      small
+                    >
+                      START RADIO
+                    </v-btn>
+                  </v-card-actions>
+                </div>
+  
+                <v-avatar
+                  class="ma-3"
+                  size="125"
+                  tile
+                >
+                  <v-img :src="item.src"></v-img>
+                </v-avatar>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding',
+      },
+    ],
+  }),
+  }
+</script>
