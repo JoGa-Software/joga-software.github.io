239df8010e1bdcdf4e11125fd8d3f74aa750aee1
Gabriel Sanchez <jogasoftware07@gmail.com>
2023-03-17 21:54:49 -0700

    try making route work

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { createBrowserRouter } from 'react-router-dom';
 
 import Games from '../pages/Games';
-import NotFound from '../pages/not-found'
-import SSCPrivacyPolicy from '../pages/SSC_Privacy_Policy'
+import NotFound from '../pages/not-found';
+import SSCPrivacyPolicy from '../pages/SSC_Privacy_Policy';
 
 const router = createBrowserRouter([
     {
